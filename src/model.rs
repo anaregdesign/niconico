c077395f54dec856996306b3075a93e8982826c0
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-07-12 08:43:39 +0900

    init

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use nannou::prelude::*;
 use nannou::text::{Font, FontSize, Justify};
 
@@ -28,7 +30,26 @@ impl MessageScreen {
     pub fn add_message(&mut self, body: &str, app: &App) {
         let v = app.window_rect().y;
         let h = app.window_rect().x;
-        let y = random_range(v.start, v.end);
+        let height = h.end - h.start;
+        // split h to 5 ranges
+        let num_part = 5;
+        let slots: Vec<Range<f32>> = (0..num_part).map(|i| {
+            (h.start + height / num_part as f32 * i as f32)..(h.start + height / num_part as f32 * (i + 1) as f32)
+        }).collect();
+        let mut counts: Vec<i32> = (0..num_part).map(|i| { 0 }).collect();
+
+        for m in &self.messages {
+            for i in 0..num_part {
+                if slots[i].start < m.p.y && m.p.y <= slots[i].end {
+                    counts[i] += 1
+                }
+            }
+        }
+
+        let min_count = counts.iter().min().unwrap();
+        let min_slot_index = counts.iter().position(|e| e == min_count).unwrap();
+        let y = random_range(slots[min_slot_index].start, slots[min_slot_index].end);
+
         let p = pt2(h.end + 400.0, y);
         self.messages.push(Message::new(p, body));
     }
@@ -38,7 +59,7 @@ impl MessageScreen {
         for message in &mut self.messages {
             message.p.x -= (h.end - h.start) / 400.0;
         }
-        self.messages.retain(|message| message.p.x > h.start - 2500.0);
+        self.messages.retain(|message| message.p.x > h.start - 1200.0);
     }
 
     pub fn draw(&self, draw: &Draw, size: f32) {
