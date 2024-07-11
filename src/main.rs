9abd6682e96ad0ca9c05479d9c9d17f8efa928e8
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-07-11 19:09:48 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use nannou::prelude::*;
+
+use model::MessageScreen;
+
+mod model;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .event(event)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {
+    message_screen: MessageScreen,
+}
+
+fn model(_app: &App) -> Model {
+    let w = _app.window_rect().x;
+    let h = _app.window_rect().y;
+    let message_screen = MessageScreen::new();
+    Model {
+        message_screen,
+    }
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {
+    match _event {
+        Event::WindowEvent { id: _id, simple: window_event } => {
+            if let Some(KeyPressed(key)) = window_event {
+                match key {
+                    Key::Up => _model.message_screen.add_message("hello", _app),
+                    _ => {}
+                }
+            }
+        }
+        _ => {}
+    }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+    _model.message_screen.update(_app);
+}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {
+    let draw = _app.draw();
+    draw.background().color(BLACK);
+
+    _model.message_screen.draw(&draw, 64.0);
+
+    draw.to_frame(_app, &_frame).unwrap();
 }
---
+++ src/model.rs
@@ -0,0 +1,50 @@
+use nannou::prelude::*;
+use nannou::text::FontSize;
+
+pub struct Message {
+    p: Point2,
+    body: String,
+}
+
+impl Message {
+    pub fn new(p: Point2, body: &str) -> Self {
+        Message { p, body: body.to_string() }
+    }
+}
+
+pub struct MessageScreen {
+    messages: Vec<Message>,
+}
+
+impl MessageScreen {
+    pub fn new() -> Self {
+        MessageScreen {
+            messages: Vec::new(),
+        }
+    }
+
+    pub fn add_message(&mut self, body: &str, app: &App) {
+        let v = app.window_rect().y;
+        let h = app.window_rect().x;
+        let y = random_range(v.start, v.end);
+        let p = pt2(h.end, y);
+        self.messages.push(Message::new(p, body));
+    }
+
+    pub fn update(&mut self, app: &App) {
+        let h = app.window_rect().x;
+        for message in &mut self.messages {
+            message.p.x -= (h.end - h.start) / 400.0;
+        }
+        self.messages.retain(|message| message.p.x > h.start);
+    }
+
+    pub fn draw(&self, draw: &Draw, size: f32) {
+        self.messages.iter().for_each(|message| {
+            draw.text(&message.body)
+                .xy(message.p)
+                .font_size(FontSize::from_f32(size).unwrap())
+                .color(WHITE);
+        });
+    }
+}
